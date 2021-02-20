3360a6853dcc76b0a4110620e1c25727d0d5969a
2007130269 <panqian@sunwoda.com>
2021-02-20 10:46:17 +0800

    react项目 基本配置webpack

---
+++ src/app.jsx
@@ -0,0 +1,20 @@
+/*
+ * @Description: 
+ * @Version: 1.0
+ * @Autor: Pandora
+ * @Date: 2021-02-09 10:05:50
+ */
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import 'font-awesome/css/font-awesome.min.css'
+import './index.css';
+import './index.scss'
+ReactDOM.render(
+  <div>
+    <i className="fa fa-snowflake-o"></i>
+    <h1>Hello, world!</h1>
+  </div>
+  ,
+  document.getElementById('app')
+)
