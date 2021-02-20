5e8faf156afb7f15eb8dab1f68f661d641cb39b6
2007130269 <panqian@sunwoda.com>
2021-02-20 11:43:34 +0800

    jsx语法

--- src/app.jsx
+++ src/app.jsx
@@ -9,12 +9,42 @@ import ReactDOM from 'react-dom';
 
 import 'font-awesome/css/font-awesome.min.css'
 import './index.css';
+import './index.scss'
+// ReactDOM.render(
+//   <div>
+//     <i className="fa fa-snowflake-o"></i>
+//     <h1>Hello, world!</h1>
+//   </div>
+//   ,
+//   document.getElementById('app')
+// )
+// 样式没有了横杠，变成了驼峰
+let style = { // 优先级比css更高一点
+  // color: 'red',
+  // fontSize: '20px',
+}
+let name = 'Pandora'
+let flag = false
+let names = ['ppq', 'pq', 'pandora']
+let jsx = (
+  <div className="jsx" style={style}>
+    {/*变量*/}
+    <p>I am {name}</p>
+    {/**条件判断 */}
+    {
+      flag ? <p>I am {name}</p> : <p>not {name}</p>
+    }
+    {/**数组循环 */}
+    {
+      names.map((name, index) => 
+        <p key={index}>Hello,I love {name}</p>
+      )
+    }
+    jsx____
+  </div>
+);
 
 ReactDOM.render(
-  <div>
-    <i className="fa fa-snowflake-o"></i>
-    <h1>Hello, world!</h1>
-  </div>
-  ,
+  jsx,
   document.getElementById('app')
 )
